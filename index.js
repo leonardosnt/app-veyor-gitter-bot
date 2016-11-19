583b2bbade242944f2bb97de48df847c6e051e86
Leonardo Santos <devleeo@outlook.com>
2016-11-19 14:08:46 -0300

    npmfy

--- index.js
+++ index.js
@@ -16,44 +16,50 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-var https = require('https');
-var process = require('process');
+'use strict'
 
-const ROOM_ID = process.env['GITTER_BOT_ROOM_ID'];
-const ACCESS_TOKEN = process.env['GITTER_BOT_ACCESS_TOKEN'];
-const MESSAGE = process.env['GITTER_BOT_MESSAGE'];
+function run() {
+  var https = require('https');
+  var process = require('process');
 
-// Ensures that the required variables must be defined
-['GITTER_BOT_ROOM_ID', 'GITTER_BOT_ACCESS_TOKEN', 'GITTER_BOT_MESSAGE']
-  .filter(v => !process.env[v])
-  .forEach(v => {
-    console.error(`Variable ${v} is not defined`);
-    process.exit();
-  })
-
-// Create request and send
-var request = https.request({
-  host: 'api.gitter.im',
-  port: 443,
-  method: 'POST',
-  path: `/v1/rooms/${ROOM_ID}/chatMessages?access_token=${ACCESS_TOKEN}`,
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-  }
-});
-
-request.write(JSON.stringify({ text: replaceVariables(MESSAGE) }));
-request.end();
-
-// Replace ENVIRORMENT VARIABLES, e.g. $PATH$, $GITTER_BOT_MESSAGE$
-function replaceVariables(text) {
-  var match = text.match(/\$(.*?)\$/g)
-  if (match) {
-    match.forEach(v => {
-      var variableName = v.substring(1, v.length - 1); // Remove '$'
-      text = text.replace(v, process.env[variableName]);
+  // Ensures that the required variables must be defined
+  ['GITTER_BOT_ROOM_ID', 'GITTER_BOT_ACCESS_TOKEN', 'GITTER_BOT_MESSAGE']
+    .filter(v => !process.env[v])
+    .forEach(v => {
+      console.error(`Variable ${v} is not defined`);
+      process.exit();
     })
+    
+  var roomId = process.env['GITTER_BOT_ROOM_ID'];
+  var accessToken = process.env['GITTER_BOT_ACCESS_TOKEN'];
+  var message = process.env['GITTER_BOT_MESSAGE'];
+
+  // Create request and send
+  var request = https.request({
+    host: 'api.gitter.im',
+    port: 443,
+    method: 'POST',
+    path: `/v1/rooms/${roomId}/chatMessages?access_token=${accessToken}`,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  });
+
+  request.write(JSON.stringify({ text: replaceVariables(message) }));
+  request.end();
+
+  // Replace ENVIRORMENT VARIABLES, e.g. $PATH$, $GITTER_BOT_MESSAGE$
+  function replaceVariables(text) {
+    var match = text.match(/\$(.*?)\$/g)
+    if (match) {
+      match.forEach(v => {
+        var variableName = v.substring(1, v.length - 1); // Remove '$'
+        text = text.replace(v, process.env[variableName]);
+      })
+    }
+    return text;
   }
-  return text;
 }
+
+module.exports = { run };
